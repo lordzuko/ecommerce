a381b55f029d6ff6893f186a01e5fa21ecd8f2a8
Himanshu Maurya <himanshumaurya2214225@hotmail.com>
2017-01-26 23:29:26 +0530

    Added Product List

--- src/products/views.py
+++ src/products/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import Product
 
-class ProductDetailView(DetailView):
+
+class ProductListView(ListView):
 	model = Product
 
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductListView, self).get_context_data(*args,**kwargs)
+		print(context)
+		return context
+
+class ProductDetailView(DetailView):
+	model = Product
+	#template_name = "<appname>/<modelname>_detail.html"
+
 # def product_detail_view_func(request, id):
-# 	product_instance = Product.objects.get(id=id)
-# 	template = "product_detail.html"
+# 	product_instance = get_object_or_404(Product,
+# 	template = "products/product_detail.html"
 # 	context	 = {
 # 		"object": product_instance
 # 	}
